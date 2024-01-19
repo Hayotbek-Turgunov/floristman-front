09275cb15d5dc1cf6effd7a696c749e261b8ee32
Hayotbek-Turgunov <hayotbekturgunov01gmail.com>
2024-01-19 11:49:48 +0500

    made initial layout

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import clasess from './Home.module.scss'
+
+const Home = () => {
+  return (
+    <div className={clasess['root']}>Home</div>
+  )
+}
+
+export default Home
