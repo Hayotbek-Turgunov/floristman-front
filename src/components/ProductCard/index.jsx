ff17de174c6720ddac583bb691cf067cfb9291da
Hayotbek-Turgunov <hayotbekturgunov01gmail.com>
2024-01-22 10:30:00 +0500

    finished data fetchin on page categories

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,30 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './ProductCard.module.scss'
 import classNames from 'classnames'
 
 
-const data = {
-  name: 'Basket No 1',
-  category: 'flower',
-  images: [
-    'https://images.squarespace-cdn.com/content/v1/5eac4ea3e88fff1b365dc45d/1599599381716-G6S1JK3EQVVQR1F4ZJ4S/AUGUST+pink+50x70.jpg?format=1000w'
-  ],
-  price: 1500000,
-  discount: 1000000
-}
-
-
-
-const ProductCard = ({ className }) => {
+const ProductCard = ({ className, data }) => {
   const [image] = data.images
+  const [selected, setSelected] = useState(false)
+  const selectItem = () => {
+    setSelected((prevState) => !prevState)
+  }
+
+
   return (
     <div className={classNames(classes['card'], className)}>
       <img src={image} alt="" className={classes['card__image']} />
       <h3 className={classes['card__title']}>{data.name}</h3>
-      <p className={classes['card__price']}>{data.price} USD<span className={classes['card__discount']}>{data.discount}</span></p>
-      <button className={classes['card__button']}>
-        ADD to Card
+      <p className={classes['card__price']}>{data.price} USD<span className={classes['card__discount']}>{data.discount}  <br />USD</span></p>
+      <button onClick={selectItem} className={classNames(classes['card__button'], selected && classes['card__button_selected'])}>
+        {selected ? 'Added' : 'Add to card'}
       </button>
+      <button></button>
     </div>
   )
 }
--- src/pages/Catagory/index.jsx
+++ src/pages/Catagory/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../components/Header'
 import { useParams } from 'react-router-dom'
 import Container from '../../layout/Container'
@@ -8,11 +8,20 @@ import classes from './Category.module.scss'
 
 const Category = () => {
   const { type } = useParams()
+  const [data, setData] = useState(null)
+
+  const fetchData = () => {
+    fetch('http://localhost:4000/products')
+      .then((data) => (data.json()))
+      .then((cards) => setData(cards))
+  }
   return (
     <>
       <Header />
       <Container className={classes['cards']}>
-        <ProductCard className={classes['card-item']} />
+        {data && data.map(card => <ProductCard data={card} key={card.id} className={classes['card-item']} />
+        )}
+        <button onClick={fetchData}>FetchData</button>
       </Container>
     </>
   )
